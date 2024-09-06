3edd5cdd9b7626421ff246f435222a0d4218e30c
scarieldias <alessandro.scariel@gmail.com>
2024-09-05 21:30:56 -0300

    Exemplos de Lista com Append

---
+++ ListaAppend.py
@@ -0,0 +1,23 @@
+listaPessoa =[]
+
+pessoa = {"nome": "Ana", "Idade": 32}
+
+listaPessoa.append(pessoa)
+
+print(listaPessoa)
+
+pessoa = {"nome": "Brenda", "Idade": 26}
+
+listaPessoa.append(pessoa)
+
+print(listaPessoa)
+
+pessoa = {"nome": "Fernanda", "Idade": 48}
+
+listaPessoa.append(pessoa)
+
+print(listaPessoa)
+
+brenda = listaPessoa[1]
+
+print(brenda)
