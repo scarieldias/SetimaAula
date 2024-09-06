cbebe95baea79e49ce446c5a0f40a86900b56df2
scarieldias <alessandro.scariel@gmail.com>
2024-09-05 21:03:54 -0300

    Exemplos de DadosCompostos

---
+++ DadosCompostos.py
@@ -0,0 +1,13 @@
+# Exemplo de listas
+
+frutas = ["maça", "banana", "laranja"]
+print("Lista de Frutas: ", frutas)
+
+# Adicionando um item
+
+frutas.append("Uva")
+print("Lista de Frutas atualizadas: ", frutas)
+
+# Acessando um item
+
+print("Primeira Frutas: ", frutas[0])
