f4a8083d804acee52241c8afa030509f84341540
scarieldias <alessandro.scariel@gmail.com>
2024-09-05 21:15:21 -0300

    Exemplos de Dicionario/Objeto

---
+++ DicionarioObjeto.py
@@ -0,0 +1,14 @@
+# Exemplo com dicionario/Objeto
+
+pessoa = {"nome": "Ana", "Idade": 30}
+
+print("Pessoa :", pessoa)
+
+# Acessando Valores
+
+print("Nome", pessoa["nome"])
+
+# Adicionando um novo par chave-valor
+
+pessoa["cidade"] = "São Paulo"
+print("Pessoa Atualizada", pessoa)
